6fece735f36495ba45c97cf3df7e5ffa65623aaa
Diilu Behera <imdilub@gmail.com>
2022-09-21 09:53:46 +0530

    Added class for CSS styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,15 @@ import React from "react"
 
 export default function Main() {
     return (
-        <div>
-        <h1>Fun facts about ReactJS</h1>
-        <ul>
+        <main>
+        <h1 className="main-title">Fun facts about ReactJS</h1>
+        <ul className="main-list">
             <li>Was first released in 2013.</li>
             <li>Was originally created by Jordan Walke.</li>
             <li>Has well over 100k stars on GitHub.</li>
             <li>Is maintained by Facebook.</li>
             <li>Powers thousands of enterprise apps, including mobile apps.</li>
         </ul>
-        </div>
+        </main>
     )
 }
