201cae54e22b462033466483f3ab2cecf2f9a73e
Diilu Behera <imdilub@gmail.com>
2022-10-28 23:56:26 +0530

    props passed for dark/light mode

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <main>
+        <main className={props.darkMode ? "dark" : ""}>
         <h1 className="main-title">Fun facts about ReactJS</h1>
         <ul className="main-list">
             <li>Was first released in 2013.</li>
